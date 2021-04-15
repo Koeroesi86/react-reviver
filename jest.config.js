8d189e961b2ea178fc97928c0828d3d42fe84a08
Krisztian Korosi <koeroesi86@gmail.com>
2021-04-15 07:47:22 +0100

    Attempt to improve pipeline with v2

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { pathsToModuleNameMapper } = require("ts-jest/utils");
 const { compilerOptions } = require("./tsconfig")
 
@@ -5,5 +6,6 @@ module.exports = {
   rootDir: "src",
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  cacheDirectory: path.resolve(__dirname, ".cache/jest/"),
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths , { prefix: '<rootDir>/' }),
 };
